021ba5f17c34b3a1334f7d51c26e5f6112b9c743
Shkutov KE <ske@npobaltros.ru>
2022-09-25 13:18:07 +0300

    Перенес картинки и изменил ссылки

--- script.js
+++ script.js
@@ -9,17 +9,33 @@ button.setAttribute('href', 'whatsapp://send?text=Обезьяна: ' + encodeUR
 //button.setAttribute('href', sharehref);
 //console.log(button);
 
-var url1 = "/img/turn_right.png";
+var url1 = "/turn_right.png";
 var url11 = location.href.replace(window.location.pathname, url1);
 document.getElementById('waButton_1').setAttribute('href', 'whatsapp://send?text=Направо: ' + encodeURIComponent(url11));
-var url2 = "/img/turn_left.png";
+var url2 = "/turn_left.png";
 var url22 = location.href.replace(window.location.pathname, url2);
 document.getElementById('waButton_2').setAttribute('href', 'whatsapp://send?text=Налево: ' + encodeURIComponent(url22));
 
+var loc = window.location.pathname;
+var dir = loc.substring(0, loc.lastIndexOf('/'));
 
-//window.location.pathname+window.location.search
+document.getElementById('s1').innerHTML = window.location;
+document.getElementById('s2').innerHTML = url1;
 
-//console.log('window.location.pathname', window.location.pathname);
-//console.log('location.href.replace', location.href.replace(window.location.pathname, url1));
 
-//location.href.replace();
+
+function getImageURI(imagePath) {
+    if (imagePath.indexOf('http') == 0) {
+        return imagePath
+    }
+    var rootPath = window.location.protocol + "//" + window.location.host + "/";
+    var path = window.location.pathname;
+    if (path.indexOf("/") == 0) {
+        path = path.substring(1);
+    }
+    path = path.split("/", 1);
+    if (path != "") {
+        rootPath = rootPath + path + "/";
+    }
+    return rootPath + imagePath;
+}
