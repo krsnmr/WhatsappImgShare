080ad6be9b1af7ffcc3d2926fd194994b958b860
Shkutov KE <ske@npobaltros.ru>
2022-09-29 20:22:18 +0300

    подправил разметку

--- src/script.js
+++ src/script.js
@@ -5,25 +5,22 @@ var waBtn3 = document.getElementById('waBtn3');
 var image_01 = document.getElementById('image_01');
 var image_02 = document.getElementById('image_02');
 var ref1 = document.getElementById('ref_page_turn_left');
-
+var ref2 = document.getElementById('ref_page_turn_right');
 
 var imageURL1 = image_01.src; // отправление ссылки на картинку
 var imageURL2 = image_02.src;
 var pageTurnLeftUrl = ref1.href;
+var pageTurnRightUrl = ref2.href;
 
-var urlPageTurnLeft = location.href.replace(location.pathname, '') + "/pages/page_turn_left.html";
+var currDate = new Date();
+var d = currDate.toLocaleString();
+
+button01.setAttribute('href', 'whatsapp://send?text=Левый поворот: на ' + d + ' ' + encodeURIComponent(imageURL1));
+button02.setAttribute('href', 'whatsapp://send?text=Правый поворот: на ' + d + ' ' + encodeURIComponent(imageURL2));
+waBtn3.setAttribute('href', 'whatsapp://send?text=Левый поворот: на' + d + ' ' + encodeURIComponent(pageTurnLeftUrl));
+waBtn4.setAttribute('href', 'whatsapp://send?text=Правый поворот: на' + d + ' ' + encodeURIComponent(pageTurnRightUrl));
 
 
-console.log('pageTurnLeftUrl', pageTurnLeftUrl);
-
-// let sharehref = `whatsapp://send?text=${encodeURIComponent(imageSrc)}`;
-
-button01.setAttribute('href', 'whatsapp://send?text=Левый поворот: ' + encodeURIComponent(imageURL1));
-button02.setAttribute('href', 'whatsapp://send?text=Правый поворот: ' + encodeURIComponent(imageURL2));
-waBtn3.setAttribute('href', 'whatsapp://send?text=Левый поворот: ' + encodeURIComponent(pageTurnLeftUrl));
-
-//button.setAttribute('href', sharehref);
-//console.log(button);
 
 var url1 = "/turn_right.png";
 var url11 = location.href.replace(window.location.pathname, url1);
